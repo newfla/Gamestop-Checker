4ecfd0ea671e9ab0fef105fceb2478fe31babee3
flavio <newfla@hotmail.it>
2019-03-16 20:05:21 +0100

    A first 'search bar' implementation

--- lib/gameLib/gamesContainer.dart
+++ lib/gameLib/gamesContainer.dart
@@ -4,16 +4,30 @@ import 'gameBuilder.dart';
 
 class GamesContainer {
 
+  //Singleton remember Dart Lazy init
+  static final GamesContainer _instance = GamesContainer._internal();
+
   //Fields
   Map<GamePlatform,List<Game>> _mapPlatformGames = Map();
   Map<GamePlatform,GameBuilder> _mapPlatformBuilder = Map();
   Future<void> _loadPage;
 
+  //Singleton factory
+  factory GamesContainer() => _instance;
+
   //Getter methods
   List<Game> getByPlatform(GamePlatform platform) => _mapPlatformGames[platform];
+  List<Game> getAll(){
+    List<Game> games = List<Game>();
+
+    for(List<Game> list in _mapPlatformGames.values)
+      games.addAll(list);
+
+    return games;
+  }
 
   //Constructor
-  GamesContainer(){
+  GamesContainer._internal(){
     
     //Start loading the site
     _loadPage = GameBuilder.loadPage();
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,55 @@
+
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:gamestop_games/gamestop.dart';
+import 'package:splashscreen/splashscreen.dart';
 
-void main() => runApp(GamesApp());
+
+void main() => runApp(MaterialApp(
+    home: GamesSplashScreen())
+);
+
+class GamesSplashScreen extends StatefulWidget{
+  @override
+  _GamesSplashScreenState createState() => _GamesSplashScreenState();
+}
+
+class _GamesSplashScreenState extends State<GamesSplashScreen>{
+  @override
+  Widget build(BuildContext context) {
+    GamesContainer();
+    return new SplashScreen(
+      seconds: 2,
+      navigateAfterSeconds: GamesApp(),
+      title: Text('GameStopChecker',
+        style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 30.0,
+            height: 2.5
+        ),
+      ),
+      image: Image(
+          image: AssetImage('images/web_hi_res_512.png')
+      ),
+      backgroundColor: Colors.white,
+      photoSize: 80.0,
+      loaderColor: Colors.red,
+    );
+  }
+}
 
 class GamesApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Contollo Validità Gamestop',
+      title: 'Gamestop Checker',
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: GamesHomePage(title: 'Contollo Validità Gamestop'),
+      home: GamesHomePage(title: 'Gamestop Checker'),
     );
   }
 }
@@ -35,14 +71,33 @@ class _GamesHomePageState extends State<GamesHomePage> {
     Tab(text: GamePlatform.SWITCH.toString().split(".")[1])
   ];
 
+  final TextEditingController _filter = new TextEditingController();
+
+  Icon _searchIcon = new Icon(Icons.search);
+  Widget _appBarTitle = new Text('Gamestop Checker');
+  String _searchtext='';
+
+
   final GamesContainer _container = GamesContainer();
 
-  void _populateContainer() {
-    _container.populate().then((_) => setState(() {}));
+  Future<void> _populateContainer() async {
+   await _container.populate();
+   setState(() {});
   }
 
   _GamesHomePageState() {
     _populateContainer();
+    _filter.addListener(() {
+      if (_filter.text.isEmpty) {
+        setState(() {
+          _searchtext = '';
+        });
+      } else {//if(_filter.text.length>2){
+        setState(() {
+          _searchtext = _filter.text;
+        });
+      }
+    });
   }
 
   @override
@@ -53,17 +108,11 @@ class _GamesHomePageState extends State<GamesHomePage> {
       length: tabs.length,
       child: Scaffold(
         appBar: AppBar(
-          title: Text(widget.title),
+          title: _appBarTitle,
           actions: <Widget>[
-            PopupMenuButton<String>(
-              onSelected: (String selected) => print(selected), //TODO
-              itemBuilder: (BuildContext context) =>
-              <PopupMenuEntry<String>>[
-                const PopupMenuItem<String>(
-                    value: "Info",
-                    child: Text("Info")
-                )
-              ],
+            IconButton(
+              icon: _searchIcon,
+              onPressed: ()=> _searchPressed()
             )
           ],
           bottom: TabBar(
@@ -75,11 +124,14 @@ class _GamesHomePageState extends State<GamesHomePage> {
             GamePlatform _platform = GamePlatform.values[GamePlatform.values
                 .indexWhere((elem) => elem.toString().contains(tab.text))];
             List<Game> _games = _container.getByPlatform(_platform);
-            return ListView.builder(
-                itemBuilder: (BuildContext context, int index) {
-                  if (_games != null && index < _games.length)
-                    return _buildRow(_games[index], _platform);
-                }
+            return RefreshIndicator(
+                child: ListView.builder(
+                  itemBuilder: (BuildContext context, int index) {
+                    if (_games != null && index < _games.length && (_searchtext.isEmpty || _games[index].title.toString().toLowerCase().contains(_searchtext.toLowerCase())))
+                      return _buildRow(_games[index], _platform);
+                  }
+                ),
+              onRefresh: _populateContainer,
             );
           }).toList(),
         ),
@@ -131,5 +183,25 @@ class _GamesHomePageState extends State<GamesHomePage> {
       await launch(url);
     else
       throw 'Could not launch $url';
+  }
+
+  _searchPressed(){
+    final String searchText = 'Cerca per';
+    setState(() {
+      if (this._searchIcon.icon == Icons.search) {
+        this._searchIcon = new Icon(Icons.close);
+        this._appBarTitle = new TextField(
+          controller: _filter,
+          decoration: new InputDecoration(
+              prefixIcon: new Icon(Icons.search),
+              hintText: searchText
+          ),
+        );
+      } else {
+        this._searchIcon = new Icon(Icons.search);
+        this._appBarTitle = new Text( widget.title );
+        _filter.clear();
+      }
+    });
   }
 }
